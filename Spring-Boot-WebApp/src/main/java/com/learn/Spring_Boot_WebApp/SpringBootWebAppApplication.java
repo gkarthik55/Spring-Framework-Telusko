04742ce77cb5ec2196c6b3099130de1b660bc92c
gkarthik05 <gkarthik.55@gmail.com>
2025-05-24 08:29:02 +0530

    Spring Boot WebApp

---
+++ Spring-Boot-WebApp/src/main/java/com/learn/Spring_Boot_WebApp/SpringBootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.learn.Spring_Boot_WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-WebApp/src/test/java/com/learn/Spring_Boot_WebApp/SpringBootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.Spring_Boot_WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
